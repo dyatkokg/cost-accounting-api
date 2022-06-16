3e1e65cac599a979e24efc37daa7dded9060aa4c
dyatkokg <dyatkokg@gmail.com>
2022-06-16 16:31:36 +0400

    first commit

---
+++ src/main/java/me/dyatkokg/costaccountingapi/CostAccountingApiApplication.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.costaccountingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostAccountingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostAccountingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dyatkokg/costaccountingapi/CostAccountingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.costaccountingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostAccountingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
